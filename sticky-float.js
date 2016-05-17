913f0944958dacea8deb59e68188ea8bfb271513
IceJOKER <admin@icejoker.ru>
2016-05-17 14:44:16 +0300

    Edit for smooth sticking - https://toster.ru/q/320372

--- sticky-float.js
+++ sticky-float.js
@@ -4,215 +4,214 @@
 
 (function( $ ){
 
-    var jWindow = jQuery(window);
+	var jWindow = jQuery(window);
 
-    // All tracked element
-    var elements = [];
+	// All tracked element
+	var elements = [];
 
-    /**
-     * Default options for element
-     * @type {{top: number, spacer: boolean, saveWidth: boolean, parent: boolean|object}}
-     */
-    var options = {
-        top: 0,           // top margin (in px)
-        spacer: true,     // create spacer
-        saveWidth: true, // save width element
-        parent: false     // change parent element
-    };
+	/**
+	 * Default options for element
+	 * @type {{top: number, spacer: boolean, saveWidth: boolean, parent: boolean|object}}
+	 */
+	var options = {
+		top: 0,           // top margin (in px)
+		spacer: true,     // create spacer
+		saveWidth: true, // save width element
+		parent: false     // change parent element
+	};
 
-    /**
-     * Default variables values for element
-     * @type {{distance: number, parent: null|jQuery, offset: number, spacer: boolean|object, stick: boolean, stop: boolean}}
-     */
-    var variables = {
-        distance: 0,
-        parent: null,
-        offset: 0,
-        spacer: false,
-        stick: false,
-        stop: false
-    };
+	/**
+	 * Default variables values for element
+	 * @type {{distance: number, parent: null|jQuery, offset: number, spacer: boolean|object, stick: boolean, stop: boolean}}
+	 */
+	var variables = {
+		distance: 0,
+		parent: null,
+		offset: 0,
+		spacer: false,
+		stick: false,
+		stop: false
+	};
 
-    var methods = {
+	var methods = {
 
-        /**
-         * Construct plugin
-         * @param args argument plugin
-         * @returns object
-         */
-        init: function ( args ) {
+		/**
+		 * Construct plugin
+		 * @param args argument plugin
+		 * @returns object
+		 */
+		init: function ( args ) {
 
-            if( !this.data('sticky') ) {
+			if( !this.data('sticky') ) {
 
-                var new_element = {
-                    object: this,
-                    options: $.extend({}, options, args),
-                    variables: $.extend({}, variables, {})
-                };
+				var new_element = {
+					object: this,
+					options: $.extend({}, options, args),
+					variables: $.extend({}, variables, {})
+				};
 
-                new_element.variables.distance = this.offset().top - new_element.options.top;
-                new_element.variables.parent   = new_element.options.parent ? new_element.options.parent : this.parent();
-                new_element.variables.offset   = new_element.variables.parent.offset().top - new_element.options.top - this.outerHeight(true);
+				new_element.variables.outer_width = this.outerWidth(true);
+				new_element.variables.distance = this.offset().top - new_element.options.top;
+				new_element.variables.parent   = new_element.options.parent ? new_element.options.parent : this.parent();
+				new_element.variables.offset   = new_element.variables.parent.offset().top - new_element.options.top - this.outerHeight(true);
 
-                if (new_element.options.spacer) {
-                    new_element.variables.spacer = $("<div />");
-                    this.before( new_element.variables.spacer );
-                }
+				if (new_element.options.spacer) {
+					new_element.variables.spacer = $("<div />");
+					this.before( new_element.variables.spacer );
+				}
 
-                this
-                    .addClass('plg-sticky')
-                    .data('sticky', true);
+				this
+					.addClass('plg-sticky')
+					.data('sticky', true);
 
-                if(!elements.length)
-                    jWindow.scroll( _scroll );
+				if(!elements.length)
+					jWindow.scroll( _scroll );
 
-                elements.push(new_element );
-            }
+				elements.push(new_element );
+			}
 
-            return this;
-        },
+			return this;
+		},
 
-        /**
-         * Destroy plugin for element
-         */
-        destroy: function () {
+		/**
+		 * Destroy plugin for element
+		 */
+		destroy: function () {
 
-            var self   = this;
+			var self   = this;
 
-            if( self.data('sticky') ) {
-                $.each(elements, function (counter, element) {
-                    if(element && element.object.get(0) === self.get(0)) {
+			if( self.data('sticky') ) {
+				$.each(elements, function (counter, element) {
+					if(element && element.object.get(0) === self.get(0)) {
 
-                        _unstick(element);
-                        elements.splice(counter, 1);
-                        self
-                            .removeClass('plg-sticky')
-                            .removeData('sticky')
-                            .off('stickyfloat');
+						_unstick(element);
+						elements.splice(counter, 1);
+						self
+							.removeClass('plg-sticky')
+							.removeData('sticky')
+							.off('stickyfloat');
 
-                        return true;
-                    }
-                });
-            }
+						return true;
+					}
+				});
+			}
 
-            if( !elements.length )
-                jWindow.off('scroll', _scroll);
-        }
-    };
+			if( !elements.length )
+				jWindow.off('scroll', _scroll);
+		}
+	};
 
-    /**
-     * Function event scroll
-     */
-    var _scroll = $.throttle(10, function () {
+	/**
+	 * Function event scroll
+	 */
+	var _scroll = $.throttle(10, function () {
 
-        if( elements.length ) {
+		if( !elements.length ) { return true; }
 
-            // window scroll
-            var scrollTop = jWindow.scrollTop();
+		// window scroll
+		var scrollTop = jWindow.scrollTop();
 
-            $.each(elements, function (counter, el) {
+		$.each(elements, function (counter, el) {
 
-                // parent height
-                var parentHeight = el.variables.parent.outerHeight();
+			// parent height
+			var parentHeight = el.variables.parent.outerHeight();
 
-                if (!el.variables.stick && !el.variables.stop && (scrollTop >= el.variables.distance)) {
-                    _stick(el);
-                } else if (el.variables.stick && scrollTop <= el.variables.distance) {
-                    _unstick(el);
-                } else if (!el.variables.stop && (scrollTop >= (parentHeight + el.variables.offset))) {
-                    _stop(el);
-                } else if (el.variables.stop  && (scrollTop <= (parentHeight + el.variables.offset))) {
-                    _stick(el);
-                }
+			if (!el.variables.stick && !el.variables.stop && (scrollTop >= el.variables.distance)) {
+				_stick(el);
+			} else if (el.variables.stick && scrollTop <= el.variables.distance) {
+				_unstick(el);
+			} else if (!el.variables.stop && (scrollTop >= (parentHeight + el.variables.offset))) {
+				_stop(el);
+			} else if (el.variables.stop  && (scrollTop <= (parentHeight + el.variables.offset))) {
+				_stick(el);
+			}
 
-            });
-        }
-    });
+		});
 
-    /**
-     * Make sticky element
-     * @param element
-     * @private
-     */
-    var _stick = function (element) {
+	});
 
-        _unstick(element);
+	/**
+	 * Make sticky element
+	 * @param element
+	 * @private
+	 */
+	var _stick = function (element) {
 
-        var elWidth = element.object.outerWidth(true);
+		_unstick(element);
 
-        if( element.options.saveWidth )
-            element.object.css({width: elWidth + 'px'});
+		if( element.options.saveWidth )
+			element.object.css({width: element.variables.outer_width + 'px'});
 
-        element.object
-            .css({top: element.options.top + 'px'})
-            .addClass('plg-sticked');
+		element.object
+			.css({top: element.options.top + 'px'})
+			.addClass('plg-sticked');
 
-        if(element.variables.spacer && element.variables.spacer.css('display') !== 'inherit' && !element.object.is(':empty')) {
-            element.variables.spacer.css({
-                width: elWidth + 'px',
-                height: element.object.outerHeight(true),
-                display: 'inherit'
-            });
-        }
+		if(element.variables.spacer && element.variables.spacer.css('display') !== 'inherit' && !element.object.is(':empty')) {
+			element.variables.spacer.css({
+				width: element.variables.outer_width + 'px',
+				height: element.object.outerHeight(true),
+				display: 'inherit'
+			});
+		}
 
-        element.variables.stick = true;
-    };
+		element.variables.stick = true;
+	};
 
-    /**
-     * Unmake sticky element
-     * @param element
-     * @returns {boolean}
-     * @private
-     */
-    var _unstick = function (element) {
+	/**
+	 * Unmake sticky element
+	 * @param element
+	 * @returns {boolean}
+	 * @private
+	 */
+	var _unstick = function (element) {
 
-        if(!element.variables.stick)
-            return false;
+		if(!element.variables.stick)
+			return false;
 
-        element.object
-            .css({top:''})
-            .removeClass('plg-sticked-stop')
-            .removeClass('plg-sticked');
+		element.object
+			.css({top:''})
+			.removeClass('plg-sticked-stop')
+			.removeClass('plg-sticked');
 
-        if( element.options.saveWidth )
-            element.object.css({width:''});
+		if( element.options.saveWidth )
+			element.object.css({width:''});
 
-        if(element.variables.spacer && !element.variables.stop && !element.object.is(':empty'))
-            element.variables.spacer.css({display: 'none'});
+		if(element.variables.spacer && !element.variables.stop && !element.object.is(':empty'))
+			element.variables.spacer.css({display: 'none'});
 
-        element.variables.stop = false;
-        element.variables.stick= false;
-    };
+		element.variables.stop = false;
+		element.variables.stick= false;
+	};
 
-    /**
-     * Stop sticky element at bottom border parent
-     * @param element
-     * @private
-     */
-    var _stop = function (element) {
+	/**
+	 * Stop sticky element at bottom border parent
+	 * @param element
+	 * @private
+	 */
+	var _stop = function (element) {
 
-        if(element.variables.parent.css('position') !== 'relative' || element.variables.parent.css('position') !== 'absolute')
-            element.variables.parent.css('position', 'relative');
+		if(element.variables.parent.css('position') !== 'relative' || element.variables.parent.css('position') !== 'absolute')
+			element.variables.parent.css('position', 'relative');
 
-        element.object
-            .addClass('plg-sticked-stop')
-            .removeClass('plg-sticked');
+		element.object
+			.addClass('plg-sticked-stop')
+			.removeClass('plg-sticked');
 
-        element.variables.stop  = true;
+		element.variables.stop  = true;
 
-        element.object.trigger('stickyfloat.stopEvent');
-    };
+		element.object.trigger('stickyfloat.stopEvent');
+	};
 
-    $.fn.stickyfloat = function( method ) {
+	$.fn.stickyfloat = function( method ) {
 
-        if ( methods[method] ) {
-            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method with name ' +  method + ' does not exist for jQuery.stickyfloat' );
-        }
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method with name ' +  method + ' does not exist for jQuery.stickyfloat' );
+		}
 
-    };
+	};
 
 })( jQuery );
